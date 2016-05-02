0ce9ed3453586f51cc09662522e0c4cb01963514
Matt Foster <matt@mfoster.co.uk>
2016-05-02 15:11:37 +0100

    endgame now aligned properly, bgr colors picked

--- index.js
+++ index.js
@@ -39,27 +39,17 @@ var newGame = function() {
 
 
 var guessChecker = function(allDataFromHTML) {
-	
-	//console.log("chosenWord = " + chosenWord);
-
 	for (i=0; i<chosenWord.length; i++){
 		if (chosenWord[i] === playerGuess){ // player made a correct guess
 
 			playerGuessedAtLeastOneLetter = true;
 			succesfulGuess(allDataFromHTML);
 			guessedLetter = i;
-			// console.log(guessedLetter);
-			// console.log(i);xvxvxvxvx
-			// console.log("Cong rats!!");
-
 			numberOfGuessedLetters++;
-			// console.log("numberOfGuessedLetters = " + numberOfGuessedLetters);
-			// console.log("wordLength = " + wordLength);
 			showAnswerRack(); // put correct letter in answer rack
 			
 		}
 	}
-
 	if (playerGuessedAtLeastOneLetter === false) { // player made an incorrect guess
 		userLives--;
 		drawNumber++;
@@ -68,15 +58,9 @@ var guessChecker = function(allDataFromHTML) {
 		userLives1.innerHTML = userLives;
 		incorrectGuessesRack();
 		theHangedMan();
-		
-
-		// console.log("Commiserations");
-		// console.log(userLives);
-		// console.log("userLives = " + userLives);
 		if (userLives <= 0) {
 			// alert("Game Over. Starting again");
 			showFailure();
-			// location.reload();
 		}
 	}
 	playerGuessedAtLeastOneLetter = false;
@@ -89,10 +73,6 @@ var guessChecker = function(allDataFromHTML) {
 		}
 };
 
-
-
-
-
 var getRandomWord = function(){ //RANDOM WORD GENERATOR
 	console.log("Notice: getRandomWord function activated");
 	//var correctGuesses = [];
@@ -102,7 +82,6 @@ var getRandomWord = function(){ //RANDOM WORD GENERATOR
 	return randomWords[e];
 };
 
-
 var buttonClicked = function (allDataFromHTML) {// TAKES THE LETTER THAT WAS CLICKED ON
 	playerGuess = allDataFromHTML.innerHTML;
 	allDataFromHTML.disabled = true;
@@ -111,21 +90,15 @@ var buttonClicked = function (allDataFromHTML) {// TAKES THE LETTER THAT WAS CLI
 	console.log("Notice: buttonClicked function activated");
 
 	guessChecker(allDataFromHTML); 
-
 };
-
-
-
 
 var start = function(){ //Overall function
 	newGame();
 	letterIntro();
 	resetGame = document.getElementById("resetgame");
 	resetGame.style.display = "none";
-	
 
 };
-
 
 var showAnswerRack = function() {
 
@@ -141,60 +114,38 @@ var	jsAnswerRack4 = document.getElementById("answerRack4");
 var	jsAnswerRack5 = document.getElementById("answerRack5");
 var	jsAnswerRack6 = document.getElementById("answerRack6");
 var	jsAnswerRack7 = document.getElementById("answerRack7");
-// var space = "";
-
-// jsAnswerRack0.innerHTML = space; // hides all the underscores on showAnswerRack
-// jsAnswerRack1.innerHTML = space;
-// jsAnswerRack2.innerHTML = space;
-// jsAnswerRack3.innerHTML = space;
-// jsAnswerRack4.innerHTML = space;
-// jsAnswerRack5.innerHTML = space;
-// jsAnswerRack6.innerHTML = space;
-
-//if chosenWord.length === 4 
-
-	// console.log("guessedLetter = " + guessedLetter + " (" + chosenWord[guessedLetter] + ")");
 	
 	if (i === 0) {
 		jsAnswerRack0.innerHTML = chosenWord[guessedLetter];
-	// console.log("jsAnswerRack0 = " + chosenWord[guessedLetter]);
 	}
 
 	else if (i === 1) {
 		jsAnswerRack1.innerHTML = chosenWord[guessedLetter];
-	// console.log("jsAnswerRack1 = " + chosenWord[guessedLetter]);
 	}
 
 	else if (i === 2) {
 		jsAnswerRack2.innerHTML = chosenWord[guessedLetter];
-	// console.log("jsAnswerRack2 = " + chosenWord[guessedLetter]);
 	}
 
 	else if (i === 3) {
 		jsAnswerRack3.innerHTML = chosenWord[guessedLetter];
-	// console.log("jsAnswerRack3 = " + chosenWord[guessedLetter]);
 	}
 
 	else if (i === 4) {
 		jsAnswerRack4.innerHTML = chosenWord[guessedLetter];
-	// console.log("jsAnswerRack4 = " + chosenWord[guessedLetter]);
 	}
 
 	else if (i === 5) {
 		jsAnswerRack5.innerHTML = chosenWord[guessedLetter];
-	// console.log("jsAnswerRack5 = " + chosenWord[guessedLetter]);
 	}
 
 	else if (i === 6) {
 		jsAnswerRack6.innerHTML = chosenWord[guessedLetter];
-	//console.log("jsAnswerRack6 = " + chosenWord[guessedLetter]);
 	}
 
 	else if (i === 7) {
 		jsAnswerRack7.innerHTML = chosenWord[guessedLetter];
-	// console.log("jsAnswerRack7 = " + chosenWord[guessedLetter]);
 	}
-
 };
 
 var lengthChecker = function() {
@@ -214,31 +165,26 @@ var	jsAnswerRack7 = document.getElementById("answerRack7");
  		jsAnswerRack7.innerHTML = space;
  		wordLength = 5;
 	}
-
 	if (chosenWord.length === 6) {
  		jsAnswerRack6.innerHTML = space;
  		jsAnswerRack7.innerHTML = space;
  		wordLength = 6;
 	}
-
 	if (chosenWord.length === 7) {
  		jsAnswerRack7.innerHTML = space;
  		wordLength = 7;
 	}
-
-
 };
-
-
-
 
 var showResetGameButton = function() {
 	resetGame = document.getElementById("resetgame");
+	resetgame1 = document.getElementById("resetBgr");
 	console.log(resetGame);
 
 	setTimeout(function(){
 		resetGame.style.opacity = 1;
 		resetGame.style.display = "block";
+		resetgame1.style.zIndex = "181";
 	},1400);
 
 };
@@ -248,38 +194,30 @@ var resetButton = function(allDataFromHTML) {
 	location.reload();
 };
 
-
 var showCongratulations = function() {
 	var congratulationScreen = document.getElementById("congratulationScreen");
-	congratulationScreen.style.display = "block";
+	congratulationScreen.className = "show";
 
 	setTimeout(function(){
 		var balloons = document.getElementById("balloons");
-		balloons.style.top = "0%";
-	},5);
+		balloons.style.top = "20%";
+	},500);
 
 	showResetGameButton();
-
-	//have a reset game button
-		// have a button in the dom appear with the option to reset the game
-
-	
 }
 
 var showFailure = function() {
 	var failureScreen = document.getElementById("failureScreen");
-	failureScreen.style.display = "block";
+		failureScreen.className = "show";
 
+	
 	setTimeout(function(){
 		var thumbsDown = document.getElementById("thumbsDown");
-		thumbsDown.style.top = "0%";
-		showResetGameButton();
+		thumbsDown.style.top = "20%";
 	},500);
-	
-	
+
+	showResetGameButton();
 }
-
-
 var theHangedMan = function() {
 	console.log("The Hanged Man");
 
@@ -287,170 +225,104 @@ var theHangedMan = function() {
 	var condemnedJs = document.getElementById("condemned" + drawNumber);
 	
 	if (drawNumber === 0){
-
 		condemnedJs.style.display = "block";
-		// condemnedJs.style.position = "relative";
 		condemnedJs.style.left = "0px";
 		condemnedJs.style.top = "325px";
 		condemnedJs.style.opacity = "1";
-
-		console.log("base");
 	}
 
 	if (drawNumber === 1){
 		condemnedJs.style.display = "block";
-		// condemnedJs.style.position = "relative";
 		condemnedJs.style.left = "0px";
 		condemnedJs.style.top = "50px";
 		condemnedJs.style.opacity = "1";
-
-		console.log("stand");
 	}
 	
 	if (drawNumber === 2){
 		condemnedJs.style.display = "block";
-		// condemnedJs.style.position = "relative";
 		condemnedJs.style.left = "17px";
 		condemnedJs.style.top = "40px";
 		condemnedJs.style.opacity = "1";
-
-		console.log("stand_top");
 	}
 	
 	if (drawNumber === 3){
 		condemnedJs.style.display = "block";
-		// condemnedJs.style.position = "relative";
 		condemnedJs.style.left = "105px";
 		condemnedJs.style.top = "39px";
 		condemnedJs.style.opacity = "1";
-
-		console.log("stand_top");
 	}	
 	
 	if (drawNumber === 4){
 		condemnedJs.style.display = "block";
-		// condemnedJs.style.position = "relative";
 		condemnedJs.style.left = "90px";
 		condemnedJs.style.top = "104px";
 		condemnedJs.style.opacity = "1";
-
-		console.log("stand_top");
 	}
 
 	if (drawNumber === 5){
 		condemnedJs.style.display = "block";
-		// condemnedJs.style.position = "relative";
 		condemnedJs.style.left = "121px";
 		condemnedJs.style.top = "150px";
 		condemnedJs.style.opacity = "1";
-
-		console.log("stand_top");
 	}
 
 	if (drawNumber === 6){
 		condemnedJs.style.display = "block";
-		// condemnedJs.style.position = "relative";
 		condemnedJs.style.left = "62px";
 		condemnedJs.style.top = "149px";
 		condemnedJs.style.opacity = "1";
-
-		console.log("stand_top");
 	}	
 	
 	if (drawNumber === 7){
 		condemnedJs.style.display = "block";
-		// condemnedJs.style.position = "relative";
 		condemnedJs.style.left = "131px";
 		condemnedJs.style.top = "130px";
 		condemnedJs.style.opacity = "1";
-
-		console.log("stand_top");
 	}
 
 	if (drawNumber === 8){
 		condemnedJs.style.display = "block";
-		// condemnedJs.style.position = "relative";
 		condemnedJs.style.left = "162px";
 		condemnedJs.style.top = "213px";
 		condemnedJs.style.opacity = "1";
-
-		console.log("stand_top");
 	}
 
 	if (drawNumber === 9){
 		condemnedJs.style.display = "block";
-		// condemnedJs.style.position = "relative";
 		condemnedJs.style.left = "144px";
-		condemnedJs.style.top = "-218px";
+		condemnedJs.style.top = "218px";
 		condemnedJs.style.opacity = "1";
-
-		console.log("stand_top");
 	}
-
-	// setTimeout(function(){
-	// 	var thumbsDown = document.getElementById("xxx");
-		
-	// },5);
-
 };
 
 var letterIntro = function() {
-	
 	if (l<26){
 		//console.log("hello");
 		var appearingLetter = document.getElementById("letter" + l);
 		appearingLetter.style.opacity = 1;
 
-
 		l++;
 		setTimeout(letterIntro, 50);	
 	}
-	
-	//console.log("appearingLetter = (see below)");
-		// console.log(appearingLetter);
-	
 };
 
-
 var incorrectGuessesRack = function() {
-
 	var jsWrongGuessRack = document.getElementById("wrongguesses");
 
 		var wrongLetter = playerGuess;
 		wrongGuessesSoFar += wrongLetter + " ";
 		wrongguesses.innerHTML = wrongGuessesSoFar;
-
 };
-
-
 
 var succesfulGuess = function(allDataFromHTML) {
-
 allDataFromHTML.style.backgroundColor = "#63FF20";
 allDataFromHTML.style.color = "black";
-
 };
-
-
-
 
 var unSuccesfulGuess = function(allDataFromHTML) {
-
 	allDataFromHTML.style.backgroundColor = "red";
 	allDataFromHTML.style.color = "#7F0000";
-	// allDataFromHTML.style.textDecoration = "line-through";
-
 };
-
-
-
-
-// var newElement = document.createElement('div');
-// newElement.className = "someClass";
-
-// document.getElementById("buttonContainer").appendChild(newElement);
-
-
 
 // To do:
 
